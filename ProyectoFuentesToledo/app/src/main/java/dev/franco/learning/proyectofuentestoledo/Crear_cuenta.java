8212f1fcca04c2e66769b0dec6f936e43c74e52e
FrancoToledo <ftoledoroa@gmail.com>
2022-10-12 16:44:47 -0300

    inicial commit

---
+++ ProyectoFuentesToledo/app/src/main/java/dev/franco/learning/proyectofuentestoledo/Crear_cuenta.java
@@ -0,0 +1,14 @@
+package dev.franco.learning.proyectofuentestoledo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Crear_cuenta extends AppCompatActivity implements Crear_cuenta_1 {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_cuenta);
+    }
+}
---
+++ ProyectoFuentesToledo/app/src/main/java/dev/franco/learning/proyectofuentestoledo/Crear_cuenta_1.java
@@ -0,0 +1,7 @@
+package dev.franco.learning.proyectofuentestoledo;
+
+import android.os.Bundle;
+
+public interface Crear_cuenta_1 {
+    void onCreate(Bundle savedInstanceState);
+}
