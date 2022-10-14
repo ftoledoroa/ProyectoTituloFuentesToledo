69cd6e6ea08e8a0256ea82dd7fb7d29d05f3f55e
FrancoToledo <ftoledoroa@gmail.com>
2022-10-14 12:52:26 -0300

    14 octubre, app funcional v0.01 creada

--- app/src/main/java/com/example/proyectotitulofuentestoledo/MainActivity.java
+++ app/src/main/java/com/example/proyectotitulofuentestoledo/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.proyectotitulofuentestoledo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // llamar a dos widgets
+        Button btIniciarSesion = findViewById(R.id.btIniciarSesion);
+        TextView tvTitulo = findViewById(R.id.tvTitulo);
+        EditText etRut = findViewById(R.id.etRut);
+        Button btCrearCuenta = findViewById(R.id.btCrearCuenta);
+        EditText etPassword = findViewById(R.id.etPassword);
+        TextView tvRecuperar = findViewById(R.id.tvOlvidopassword);
+
+        btIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String rut = etRut.getText().toString();
+                String pass = etPassword.getText().toString();
+
+                if(rut.equals("")|| pass.equals((""))){
+                    Toast.makeText(getApplicationContext(),"Ingrese todos los Campos", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Toast.makeText(getApplicationContext(),"Aún no se desarrolla la pantalla de bienvenida", Toast.LENGTH_LONG).show();
+                //Intent i = new Intent(MainActivity.this,HomeActivity.class);
+            }
+        });
+
+        btCrearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,RegistrarCuenta.class);
+                startActivity(i);
+            }
+
+        });
+
+        tvRecuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Aún no se desarrolla la pantalla de recuperación", Toast.LENGTH_LONG).show();
+                //Intent i = new Intent(MainActivity.this,ConfirmarPasswordActivity.class);
+                //startActivity(i);
+            }
+
+        });
     }
 }
