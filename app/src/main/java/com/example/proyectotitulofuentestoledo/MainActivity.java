ca44117f45f43d1cd5519d6604e7347f569da8d7
Bastianrfuentes <bfuentes1996@gmail.com>
2022-10-17 19:04:37 -0300

    Actualización MainActivity

--- app/src/main/java/com/example/proyectotitulofuentestoledo/MainActivity.java
+++ app/src/main/java/com/example/proyectotitulofuentestoledo/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Hola a todos
+
         btCrearCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
