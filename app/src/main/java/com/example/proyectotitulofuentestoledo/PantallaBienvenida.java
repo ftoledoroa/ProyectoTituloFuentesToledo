d9a2d7d9afc94fd0910512589d04f2bb15077e06
ftoledoroa <112037253+ftoledoroa@users.noreply.github.com>
2022-10-25 13:29:09 -0300

    Actualizacion rama Fuentes (#8)
    Co-authored-by: Bastianrfuentes <bfuentes1996@gmail.com>

--- app/src/main/java/com/example/proyectotitulofuentestoledo/PantallaBienvenida.java
+++ app/src/main/java/com/example/proyectotitulofuentestoledo/PantallaBienvenida.java
@@ -2,7 +2,13 @@ package com.example.proyectotitulofuentestoledo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 
 public class PantallaBienvenida extends AppCompatActivity {
 
@@ -11,4 +17,19 @@ public class PantallaBienvenida extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_bienvenida);
     }
+
+    @SuppressLint("ResourceType")
+    public void qrPoppup(View v){
+        ImageView qr = findViewById(R.id.ivLogoQR);
+        PopupMenu pm = new PopupMenu(this, qr);
+        pm.getMenuInflater().inflate(R.layout.activity_qrpopup,pm.getMenu());
+        pm.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+    }
+
+
 }
