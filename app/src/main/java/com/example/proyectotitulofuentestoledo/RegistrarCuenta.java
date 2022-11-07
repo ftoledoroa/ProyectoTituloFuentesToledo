f5647c41a7a09e351e45e7b8df08781289e0791e
FrancoToledo <ftoledoroa@gmail.com>
2022-11-07 16:21:09 -0300

    actualizacion: funcionalidad RegistrarCuenta

--- app/src/main/java/com/example/proyectotitulofuentestoledo/RegistrarCuenta.java
+++ app/src/main/java/com/example/proyectotitulofuentestoledo/RegistrarCuenta.java
@@ -12,15 +12,27 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.proyectotitulofuentestoledo.modelo.Usuario;
+import com.example.proyectotitulofuentestoledo.modelo.Usuario;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrarCuenta extends AppCompatActivity {
     private FirebaseAuth mAuth;
+// Access a Cloud Firestore instance from your Activity
+
+    FirebaseFirestore mDB = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +40,8 @@ public class RegistrarCuenta extends AppCompatActivity {
         setContentView(R.layout.activity_registrar_cuenta);
         FirebaseApp.initializeApp(this);
         // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
+        this.mAuth = FirebaseAuth.getInstance();
+        this.mDB = FirebaseFirestore.getInstance();
 
         // llamar a dos widgets
         EditText etNombre = findViewById(R.id.etNombre);
@@ -39,41 +52,71 @@ public class RegistrarCuenta extends AppCompatActivity {
         EditText etRePassword = findViewById(R.id.etRePassword);
         Button btRegistrar = findViewById(R.id.btRegistrar);
 
-
         btRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String correo = etCorreo.getText().toString();
                 String pass = etPassword.getText().toString();
+                String rePass = etRePassword.getText().toString();
+                //String email = mAuth.getCurrentUser().getEmail();
+                String apellido = etApellido.getText().toString();
+                String nombre = etNombre.getText().toString();
+                String rut = etRut.getText().toString();
+                String password = etPassword.getText().toString();
 
+                //verificar si usuario y contraseña son no vacios
                 if(correo.equals("")|| pass.equals((""))){
                     Toast.makeText(getApplicationContext(),"Ingrese todos los Campos", Toast.LENGTH_LONG).show();
                     return;
                 }
+                //verificar si las contraseñas son iguales
+                if(pass.equals(rePass)){
+                    //crear usuario nuevo en mAuth
+                    mAuth.createUserWithEmailAndPassword(correo, pass)
+                            .addOnCompleteListener(RegistrarCuenta.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d("FIREBASE", "createUserWithEmail:success");
+                                        //FirebaseUser user = mAuth.getCurrentUser();
+                                        //updateUI(user);
+                                        //creando nuevo documento en la bd
+                                        Usuario usuario = new Usuario (apellido, nombre, correo, rut, password, 1, 1);
+                                        //mDB.collection("usuarios").add(usuario); // para añadir a la base de datos
 
-                //añadir linea para verificar que las contraseñas sean iguales
-                mAuth.createUserWithEmailAndPassword(correo, pass)
-                        .addOnCompleteListener(RegistrarCuenta.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d("FIREBASE", "createUserWithEmail:success");
-                                    //FirebaseUser user = mAuth.getCurrentUser();
-                                    //updateUI(user);
-
-                                    Intent i = new Intent(RegistrarCuenta.this,MainActivity.class);
-                                    startActivity(i);
-
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w("FIREBASE", "createUserWithEmail:failure", task.getException());
-                                    Toast.makeText(RegistrarCuenta.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                    //updateUI(null);
+                                        mDB.collection("usuarios")
+                                                .add(usuario)
+                                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                    @Override
+                                                    public void onSuccess(DocumentReference documentReference) {
+                                                        Log.d(String.valueOf(RegistrarCuenta.this), "DocumentSnapshot added with ID: " + documentReference.getId());
+                                                        Toast.makeText(RegistrarCuenta.this, "Nuevo Usuario Regirstrado", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Log.w(String.valueOf(RegistrarCuenta.this), "Error adding document", e);
+                                                        Toast.makeText(RegistrarCuenta.this, "Error al añadir usuario", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+                                        Intent i = new Intent(RegistrarCuenta.this, MainActivity.class);
+                                        startActivity(i);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w("FIREBASE", "createUserWithEmail:failure", task.getException());
+                                        Toast.makeText(RegistrarCuenta.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                        //updateUI(null);
+                                    }
                                 }
-                            }
-                        });
+                            });
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Las contraseñas son incorrectas", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
             }
         });
     }
--- app/src/main/java/com/example/proyectotitulofuentestoledo/modelo/Usuario.java
+++ app/src/main/java/com/example/proyectotitulofuentestoledo/modelo/Usuario.java
@@ -3,24 +3,27 @@ package com.example.proyectotitulofuentestoledo.modelo;
 import com.google.firebase.firestore.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
-public class usuario {
+public class Usuario {
 
     public String apellido;
     public String nombre;
     public String correo;
     public String rut;
     public String password;
-    public Integer metodo_pago;
+    public Integer metodoPago;
     public Integer privilegios;
 
+    public Usuario() {
+        //Default contructor required for calls to DataSnapshot.get
+    }
 
-    public usuario(String apellido, String nombre, String correo, String rut, String password, Integer metodo_pago, Integer privilegios) {
+    public Usuario(String apellido, String nombre, String correo, String rut, String password, Integer metodoPago, Integer privilegios) {
         this.apellido=apellido;
         this.nombre=nombre;
         this.correo=correo;
         this.rut=rut;
         this.password=password;
-        this.metodo_pago=metodo_pago;
+        this.metodoPago=metodoPago;
         this.privilegios=privilegios;
     }
 
