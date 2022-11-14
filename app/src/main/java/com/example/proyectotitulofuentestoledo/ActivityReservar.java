ab84fca87ecd80c509a262b9fd9bedb4a0a376e3
ftoledoroa <112037253+ftoledoroa@users.noreply.github.com>
2022-11-14 19:28:17 -0300

    Franco (#20)
    * actualizacion:
    creacion clase usuario
    
    * actualizacion:
    funcionalidad RegistrarCuenta
    
    * push prueba
    
    * jksdhfjasdf
    
    * usuario a;adido
    
    * fagfgfag
    
    * actualizacion:
    pantalla lista estacionamientos
    
    * actualizacion:
    pantalla lista estacionamientos v1

--- app/src/main/java/com/example/proyectotitulofuentestoledo/ActivityReservar.java
+++ app/src/main/java/com/example/proyectotitulofuentestoledo/ActivityReservar.java
@@ -4,7 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class ActivityReservar extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+// Access a Cloud Firestore instance from your Activity
+
+    FirebaseFirestore mDB = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
