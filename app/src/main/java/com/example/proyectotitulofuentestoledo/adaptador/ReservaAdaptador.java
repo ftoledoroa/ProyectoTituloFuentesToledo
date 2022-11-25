7295274abd9cfe48da05152a4a931ec1e53b8b40
FrancoToledo <ftoledoroa@gmail.com>
2022-11-25 15:47:44 -0300

    Añadida funcion de crear registro reserva

--- app/src/main/java/com/example/proyectotitulofuentestoledo/adaptador/ReservaAdaptador.java
+++ app/src/main/java/com/example/proyectotitulofuentestoledo/adaptador/ReservaAdaptador.java
@@ -1,6 +1,7 @@
 package com.example.proyectotitulofuentestoledo.adaptador;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,20 +14,33 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.proyectotitulofuentestoledo.R;
 import com.example.proyectotitulofuentestoledo.modelo.Estacionamiento;
+import com.example.proyectotitulofuentestoledo.modelo.RegistroReserva;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class ReservaAdaptador extends FirestoreRecyclerAdapter<Estacionamiento, ReservaAdaptador.ViewHolder> {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+public class ReservaAdaptador extends FirestoreRecyclerAdapter<Estacionamiento, ReservaAdaptador.ViewHolder> {
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
     FirebaseFirestore mDB = FirebaseFirestore.getInstance();
     Activity activity;
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat;
+    private String horaReserva;
+    private String idEstacionamiento;
+    String userId = mAuth.getCurrentUser().getUid();
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView numero, status, fecha;
         ImageView selectEstacionamiento;
+
 
         public ViewHolder(@NonNull View itemView){
             super(itemView);
@@ -53,17 +67,59 @@ public class ReservaAdaptador extends FirestoreRecyclerAdapter<Estacionamiento, 
     protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Estacionamiento estacionamiento) {
         DocumentSnapshot dSnapshot = getSnapshots().getSnapshot(holder.getAdapterPosition());
         final String id = dSnapshot.getId();
+        String idEstacionamiento = id;
+        calendar = Calendar.getInstance();
         holder.numero.setText(estacionamiento.getNumero());
         holder.status.setText(estacionamiento.getStatus());
         //Log.d("prueba",estacionamiento.getFecha());
         //holder.fecha.setText(estacionamiento.getFecha());
-
-
         holder.selectEstacionamiento.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {selectEstacionamiento(id);}
+            public void onClick(View view) {selectEstacionamiento(id,idEstacionamiento);}
         });
     }
+
+    private void selectEstacionamiento(String id, String idEstacionamiento) {
+        dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        horaReserva = dateFormat.format(calendar.getTime());
+
+        mDB.collection("estacionamientos").document(id).update("status", "No Disponible").addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Toast.makeText(activity, "Reservado Correctamente!, Te esperamos en el local.", Toast.LENGTH_SHORT).show();
+                RegistroReserva registroReserva = new RegistroReserva (userId, idEstacionamiento, horaReserva);
+                mDB.collection("registro_reserva")
+                        .add(registroReserva)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(String.valueOf(ReservaAdaptador.this), "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(String.valueOf(ReservaAdaptador.this), "Error adding document", e);
+                            }
+                        });
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(activity, "Error.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    @NonNull
+    @Override
+    public ReservaAdaptador.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.reservar_adaptador, parent, false);
+        return new ViewHolder(view);
+    }
+
 
     /*public void onCheckboxClicked(View view) {
         // Is the view now checked?
@@ -89,32 +145,6 @@ public class ReservaAdaptador extends FirestoreRecyclerAdapter<Estacionamiento, 
 // se necesita al activar el checkbox crear un nuevo registro en la BD con la hora  y que el estado del estacionamiento cambie a no disponible.
     // ademas se necesita asociar al usuario con el estacionamiento reservado.
 //para actualizar el estado revisar para seleccionar el estado
-
-
-    private void selectEstacionamiento(String id) {
-        mDB.collection("estacionamientos").document(id).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void unused) {
-                Toast.makeText(activity, "eliminado correctamente!.", Toast.LENGTH_SHORT).show();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(activity, "Error.", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-
-    @NonNull
-    @Override
-    public ReservaAdaptador.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.reservar_adaptador, parent, false);
-        return new ViewHolder(view);
-    }
-
-
-
 
 
 
---
+++ app/src/main/java/com/example/proyectotitulofuentestoledo/modelo/RegistroReserva.java
@@ -0,0 +1,37 @@
+package com.example.proyectotitulofuentestoledo.modelo;
+
+public class RegistroReserva {
+    private String userId;
+    private String idEstacionamiento;
+    private String horaReserva;
+
+    public RegistroReserva(String userId, String idEstacionamiento, String horaReserva) {
+        this.userId = userId;
+        this.idEstacionamiento = idEstacionamiento;
+        this.horaReserva = horaReserva;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getIdEstacionamiento() {
+        return idEstacionamiento;
+    }
+
+    public void setIdEstacionamiento(String idEstacionamiento) {
+        this.idEstacionamiento = idEstacionamiento;
+    }
+
+    public String getHoraReserva() {
+        return horaReserva;
+    }
+
+    public void setHoraReserva(String horaReserva) {
+        this.horaReserva = horaReserva;
+    }
+}
